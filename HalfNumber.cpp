7035f8e9cb7dddc7ac3e2d588880585d2190494f
Ahmad sindi <ahmadaalsindi@gmail.com>
2025-11-01 18:36:50 +0300

    Create HalfNumber.cpp

---
+++ HalfNumber.cpp
@@ -0,0 +1,16 @@
+// Algorithm Challenge #7 - Half Number
+#include <iostream>
+using namespace std;
+
+int main() {
+    float num;
+
+    cout << "Type the number: ";
+    cin >> num;
+
+    float halfNum = num / 2;
+
+    cout << "\nHalf of " << num << " is " << halfNum << endl;
+
+    return 0;
+}
